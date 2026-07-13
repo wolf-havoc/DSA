6ff0fadfc16c76a83d16a9abcc615b711a30a22c
wolf-havoc <yadavdivij@gmail.com>
2026-07-13 23:41:41 +0530

    Time: 0 ms (100%), Space: 10.8 MB (23.4%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -2,15 +2,14 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
        int n = nums.size();
-       if(n == 1) return nums[0];
-       int prev2 = nums[0];
-       int prev1 = max(nums[0],nums[1]);
-       int sum;
-       for(int i = 2;i<n;i++){
-          sum = max(prev1, prev2 + nums[i]);
-          prev2 = prev1;
-          prev1 = sum;
-       } 
-       return prev1;
+       //dp[i] = max amoutn of money robbed till ith day
+       vector<int> dp(n);
+       if(n == 1) return dp[0] = nums[0];
+       dp[0] = nums[0];
+       dp[1] = max(nums[0],nums[1]);
+       for(int i=2;i<n;i++){
+        dp[i] = max(dp[i-1],dp[i-2]+nums[i]);
+       }
+       return dp[n-1];
     }
 };
